b2dda2e32378ed8f2d4d99134b0b8153f7819fe2
Daniel González Robayna <gonrobd@gmail.com>
2019-10-26 19:56:19 +0200

    Prefab de UIManager y el map canvas de prueba.

---
+++ Assets/Scripts/ProjectScripts/Managers & Systems/MapBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MapBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<GameObject> unchartedRooms;
+    public List<GameObject> discoveredRooms;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ProjectScripts/Managers & Systems/UIManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public MapBehaviour map;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            ShowMap();
+        }
+    }
+    public void ShowMap()
+    {     
+        map.GetComponent<Canvas>().enabled = !map.GetComponent<Canvas>().enabled;
+    }
+    
+}
