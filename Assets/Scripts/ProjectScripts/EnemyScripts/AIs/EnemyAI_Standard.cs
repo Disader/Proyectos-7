4d46715b842f3fbccbd077dbf7abb7dec1dc791d
Disader <sssergiosaez@gmail.com>
2019-10-27 12:50:06 +0100

    Movimiento IA enemiga y primera máquina de estados.

--- Assets/Scripts/ProjectScripts/EnemyScripts/AIs/EnemyAI_Standard.cs
+++ Assets/Scripts/ProjectScripts/EnemyScripts/AIs/EnemyAI_Standard.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAI_Standard : MonoBehaviour
 {
@@ -10,7 +11,11 @@ public class EnemyAI_Standard : MonoBehaviour
 
     [SerializeField] Transform m_childSprite;
 
-
+    NavMeshAgent m_AI_Controller;
+    private void Start()
+    {
+        m_AI_Controller = GetComponent<NavMeshAgent>();
+    }
     enum AIState
     {
         idle,
@@ -26,35 +31,39 @@ public class EnemyAI_Standard : MonoBehaviour
         {
             DetectPlayer();
         }
-
-        RandomPointOnCircleEdge(m_distanceToPlayer);
-
+        else if(currentAIState == AIState.attackingPlayer)
+        {
+            Movement();
+        }      
         //Asegurarse de girar al final, tras haber hecho los cálculos del pathfinding
-        m_childSprite.localRotation=Quaternion.Euler(new Vector3(90, 0, 0));
+        m_childSprite.rotation=Quaternion.Euler(new Vector3(0, 0, 0));
     }
     void DetectPlayer()
     {
         Vector2 vectorToPlayer = GameManager.Instance.ActualPlayerController.transform.position - this.transform.position;
         float distanceToPlayer= vectorToPlayer.magnitude;
-        bool hasPlayerOnSight;
-        if (Physics2D.Raycast(transform.position, vectorToPlayer, distanceToPlayer, m_sightCollisionMask))
+        bool hasPlayerOnSight = !Physics2D.Raycast(transform.position, vectorToPlayer, distanceToPlayer, m_sightCollisionMask);
+       
+        Debug.DrawRay(transform.position, vectorToPlayer.normalized * distanceToPlayer, Color.red,1f);
+        if (distanceToPlayer<m_playerDetectionDistance||hasPlayerOnSight)
         {
-            hasPlayerOnSight = true;
-        }
-        else
-        {
-            hasPlayerOnSight = false;
-        }
-        Debug.DrawRay(transform.position, vectorToPlayer * distanceToPlayer, Color.red);
-
-        if (distanceToPlayer>m_playerDetectionDistance||hasPlayerOnSight)
-        {
-            currentAIState = AIState.attackingPlayer;
+           currentAIState = AIState.attackingPlayer;
         }
     }
-    private Vector3 RandomPointOnCircleEdge(float radius)
+    float m_AIClockNewDestination=2;
+
+    void Movement()
     {
-        var vector2 = Random.insideUnitCircle.normalized * radius;
+        m_AIClockNewDestination += Time.deltaTime;
+        if (m_AIClockNewDestination > 4f || m_AI_Controller.remainingDistance < m_AI_Controller.stoppingDistance)
+        {
+            m_AIClockNewDestination = 0;
+            m_AI_Controller.destination = FindDestination(m_distanceToPlayer)+ GameManager.Instance.ActualPlayerController.transform.position;
+        }
+    }
+    private Vector3 FindDestination(float playerDistance)
+    {
+        Vector2 vector2 = Random.insideUnitCircle.normalized * playerDistance;
         return new Vector3(vector2.x, vector2.y, 0);
     }
 }
