2de0e4c994cff26d547590c8ef147641f922c090
Disader <sssergiosaez@gmail.com>
2019-10-30 14:01:20 +0100

    Activación de enemigos al entrar y salir de salas y funcionalidad para poder llevarlos de una habitación a otra.

--- Assets/Scripts/ProjectScripts/Managers & Systems/RoomManager.cs
+++ Assets/Scripts/ProjectScripts/Managers & Systems/RoomManager.cs
@@ -6,14 +6,72 @@ using Cinemachine;
 public class RoomManager : MonoBehaviour
 {
     [SerializeField] CinemachineVirtualCamera m_roomCamera;
+    List<EnemyControl_MovementController> enemiesInRoom = new List<EnemyControl_MovementController>();
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.GetComponent<PlayerControl_MovementController>() == GameManager.Instance.ActualPlayerController)
         {
-            ZoneManager.Instance.SetActiveCamera(m_roomCamera);
-            m_roomCamera.gameObject.SetActive(true);
+            ChangeCamera();
+            ZoneManager.Instance.SetNewActiveRoom(this);
+           
         }
-       
+        //Set de los enemigos en la habitación al principio de la partida en el trigger, quizás un poco inestable pero fácil implementación de diseño
+        if (collision.GetComponent<EnemyControl_MovementController>()!=null)
+        {
+            AddEnemyAtRoom(collision.GetComponent<EnemyControl_MovementController>());     
+        }
+    }
+    public void AddEnemyAtRoom(EnemyControl_MovementController newEnemy)
+    {
+        if (!enemiesInRoom.Contains(newEnemy))
+        {
+            enemiesInRoom.Add(newEnemy);
+        }   
+    }
+    public void RemoveEnemyAtRoom(EnemyControl_MovementController deleteEnemy)
+    {
+        if (enemiesInRoom.Contains(deleteEnemy))
+        {
+            enemiesInRoom.Remove(deleteEnemy);
+        }
+    }
+    void ChangeCamera()
+    {
+        m_roomCamera.gameObject.SetActive(true);
+        ZoneManager.Instance.SetActiveCamera(m_roomCamera);       
+    }
+    public void ActivateEnemies()
+    {
+        foreach (EnemyControl_MovementController enemy in enemiesInRoom)
+        {
+            enemy.gameObject.SetActive(true);
+        }
+    }
+
+    List<EnemyControl_MovementController> enemiesToDelete = new List<EnemyControl_MovementController>();
+    public void DeactivateEnemies()
+    {
+        foreach (EnemyControl_MovementController enemy in enemiesInRoom)
+        {
+            if(enemy != GameManager.Instance.ActualPlayerController)
+            {
+                enemy.gameObject.SetActive(false);
+            }
+            else
+            {
+                enemiesToDelete.Add(enemy);
+            }
+        }
+        foreach (EnemyControl_MovementController enemy in enemiesToDelete)
+        {
+            enemiesInRoom.Remove(enemy);
+        }
+        enemiesToDelete.Clear();
+    }
+    void ResetRoom()
+    {
+        
     }
 
 
--- Assets/Scripts/ProjectScripts/Managers & Systems/ZoneManager.cs
+++ Assets/Scripts/ProjectScripts/Managers & Systems/ZoneManager.cs
@@ -8,16 +8,30 @@ public class ZoneManager : TemporalSingleton<ZoneManager>
     public MapBehaviour mapManager;
     public List<GameObject> rooms;
     CinemachineVirtualCamera m_activeCamera;
+    RoomManager m_activeRoom;
     void DiscoverRoom(GameObject currentRoom)
     {
         mapManager.DiscoverRoom(currentRoom);
     }
     public void SetActiveCamera(CinemachineVirtualCamera newCamera)
     {
-        if (m_activeCamera != null)
+        if (m_activeCamera != null && m_activeCamera!=newCamera)
         {
             m_activeCamera.gameObject.SetActive(false);
         }
         m_activeCamera = newCamera;
+    }
+    public void SetNewActiveRoom(RoomManager newRoom)
+    {
+        if (m_activeRoom != newRoom)
+        {
+            if (m_activeRoom != null)
+            {
+                m_activeRoom.DeactivateEnemies();
+            }
+            m_activeRoom = newRoom;
+            m_activeRoom.ActivateEnemies();
+        }
+
     }
 }
