a8010e4b6a8a98604d5bb9946fa5a055f9afccde
Disader <sssergiosaez@gmail.com>
2019-11-03 12:45:32 +0100

    Finalización del changer de habitaciones.

--- Assets/Scripts/ProjectScripts/Managers & Systems/RoomsManagers/RoomChangerObject.cs
+++ Assets/Scripts/ProjectScripts/Managers & Systems/RoomsManagers/RoomChangerObject.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public class RoomChangerObject : MonoBehaviour
 {
-   
+    [SerializeField] Transform m_rightTransform;
+    [SerializeField] Transform m_leftTransform;
+
+
+    public bool m_isSaveRoomChanger;
+
+    public void MovePlayerFromRight(GameObject player)
+    {
+        player.transform.position = m_leftTransform.position;  
+    }
+    public void MovePlayerFromLeft(GameObject player)
+    {
+        player.transform.position = m_rightTransform.position;
+    }
 }
---
+++ Assets/Scripts/ProjectScripts/Managers & Systems/RoomsManagers/RoomChangerTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomChangerTrigger : MonoBehaviour
+{
+    [SerializeField] bool imRight;
+    [SerializeField] RoomChangerObject roomChanger;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<PlayerControl_MovementController>() == GameManager.Instance.ActualPlayerController && !roomChanger.m_isSaveRoomChanger)
+        {
+            if (imRight)
+            {
+                roomChanger.MovePlayerFromRight(collision.gameObject);
+            }
+            else
+            {
+                roomChanger.MovePlayerFromLeft(collision.gameObject);
+            }
+        }
+        if (collision.GetComponent<EnemyControl_MovementController>() ==null && roomChanger.m_isSaveRoomChanger)
+        {
+            if (imRight)
+            {
+                roomChanger.MovePlayerFromRight(collision.gameObject);
+            }
+            else
+            {
+                roomChanger.MovePlayerFromLeft(collision.gameObject);
+            }
+        }
+    }
+}
