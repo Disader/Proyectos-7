188c98b8341af6de5f83f72e1819c6788d83184c
Daniel González Robayna <gonrobd@gmail.com>
2019-11-04 12:59:54 +0100

    He implementado el CutsceneManager como singleton con un método para reproducir cutscenes pasando 2 variables. Estas son un int (en caso de querer reproducir simplemente por orden) o pasando un TimelineAsset (en caso de buscar una en concreto). Además de haber un CutsceneTrigger como prefab para las cutscenes activadas por proximidad. Resta considerar que ocurre con el juego mientras una cutscene se reproduce.

---
+++ Assets/CutsceneManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
+using UnityEngine;
+
+public class CutsceneManager : TemporalSingleton<CutsceneManager>
+{
+    public List<TimelineAsset> cutscenes;
+    public PlayableDirector director;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void PlayCutscene(int cutsceneNum, TimelineAsset playable)
+    {
+        if(cutsceneNum == 0)
+        {
+            director.playableAsset = playable;
+        }
+        else
+        {
+            director.playableAsset = cutscenes[cutsceneNum];
+        }
+        director.Play();
+
+    }
+}
---
+++ Assets/CutsceneTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Timeline;
+using UnityEngine;
+
+public class CutsceneTrigger : MonoBehaviour
+{
+    [Header("Playable a reproducir")]
+    public TimelineAsset playable;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer == 8 && playable != null)
+        {
+            CutsceneManager.Instance.PlayCutscene(0, playable);
+            this.gameObject.SetActive(false);
+        }
+    }
+}
