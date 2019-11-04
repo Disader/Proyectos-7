ff0e1f535357e3704876bac441ee6c9f2b1aeba7
Daniel González Robayna <gonrobd@gmail.com>
2019-11-04 14:35:55 +0100

    Primera iteración de las cajas destructibles.

---
+++ Assets/Scripts/ProjectScripts/DestructibleObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructibleObject : MonoBehaviour
+{
+    public Sprite destroyed;
+    SpriteRenderer m_rend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rend = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+ 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 10 || collision.gameObject.layer == 12)
+        {
+            m_rend.sprite = destroyed;
+        }
+    }
+}
