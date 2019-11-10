de517da9bc5735ecb3d81d74f6cadefd012c0854
Disader <sssergiosaez@gmail.com>
2019-11-10 11:57:05 +0100

    SortingLayerChanger y arreglos en el prefab del enemigo para que funcione la IA.

---
+++ Assets/Scripts/ProjectScripts/Managers & Systems/SetOrderInLayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetOrderInLayer : MonoBehaviour
+{
+    [SerializeField] Transform transformRef;
+    SpriteRenderer mySprite;
+    private void Start()
+    {
+        mySprite = GetComponent<SpriteRenderer>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        mySprite.sortingOrder = -Mathf.RoundToInt(transformRef.position.y);
+    }
+}
