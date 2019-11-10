f1cefd9f22e5f0faf36f8b02147f3847dd1aeb98
Disader <sssergiosaez@gmail.com>
2019-11-10 13:40:16 +0100

    Implementación animación de apuntado en el enemigo.

---
+++ Assets/Scripts/ProjectScripts/EnemyScripts/SetArmAnimationValues.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetArmAnimationValues : MonoBehaviour
+{
+    Animator myAnimator;
+    [SerializeField] Transform m_transformRef;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myAnimator.SetFloat("Angle", m_transformRef.localEulerAngles.z);
+    }
+}
