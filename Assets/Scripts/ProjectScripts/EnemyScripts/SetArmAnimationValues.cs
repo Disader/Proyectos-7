f1df9f0070c5db0e0dbbe29c3a9fa7f28e935e1a
Disader <sssergiosaez@gmail.com>
2019-11-14 12:53:05 +0100

    Arreglo order in layer del arma del enemigo.

--- Assets/Scripts/ProjectScripts/EnemyScripts/SetArmAnimationValues.cs
+++ Assets/Scripts/ProjectScripts/EnemyScripts/SetArmAnimationValues.cs
@@ -5,17 +5,27 @@ using UnityEngine;
 public class SetArmAnimationValues : MonoBehaviour
 {
     Animator myAnimator;
+    SpriteRenderer mySpriteRenderer;
     [SerializeField] Transform m_transformRef;
     // Start is called before the first frame update
     void Start()
     {
         myAnimator = GetComponent<Animator>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(m_transformRef.localEulerAngles.z);
+        if (m_transformRef.localEulerAngles.z< 337.5f && m_transformRef.localEulerAngles.z > 202.5f)
+        {
+            mySpriteRenderer.sortingOrder = 1000;
+        }
+        else
+        {
+            mySpriteRenderer.sortingOrder = -1000;
+        }
         myAnimator.SetFloat("Angle", m_transformRef.localEulerAngles.z);
+
     }
 }
