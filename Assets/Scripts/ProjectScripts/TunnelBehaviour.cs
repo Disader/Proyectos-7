81d4de1f0a7ace6717be01a590697f4dd02e47cc
Daniel González Robayna <gonrobd@gmail.com>
2019-11-15 23:44:40 +0100

    Añado prefab para bloquear túneles.

---
+++ Assets/Scripts/ProjectScripts/TunnelBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TunnelBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    BoxCollider2D m_TunnelBlock;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<EnemySetControl>() != null)
+        {
+            m_TunnelBlock.enabled = true;
+        }
+        else 
+        {
+            m_TunnelBlock.enabled = false;
+        }
+            
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        m_TunnelBlock.enabled = false;   
+    }
+}
