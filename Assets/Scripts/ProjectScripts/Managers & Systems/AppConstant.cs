b824ddb6f5dde9ef184fa2d8f6eadd0029e8468b
Daniel González Robayna <gonrobd@gmail.com>
2019-11-15 17:58:24 +0100

    Añado a la librería de sonidos para que puedan ser llamados en código.

--- Assets/Scripts/ProjectScripts/Managers & Systems/AppConstant.cs
+++ Assets/Scripts/ProjectScripts/Managers & Systems/AppConstant.cs
@@ -27,6 +27,26 @@ public class AppSounds
 	public static readonly string	GAME_MUSIC       = "GameMusic";
     public static readonly string	BUTTON_SFX       = "ButtonEffect";
     public static readonly string   DOOR_SFX         = "Door_Close";
+    public static readonly string   PLAYER_MOV1      = "Player_Mov1_Aud";
+    public static readonly string   PLAYER_MOV2      = "Player_Mov2_Aud";
+    public static readonly string   PLAYER_MOV3      = "Player_Mov3_Aud";
+    public static readonly string   PLAYER_MOV4      = "Player_Mov4_Aud";
+    public static readonly string   PLAYER_MOV5      = "Player_Mov5_Aud";
+    public static readonly string   PLAYER_MOV6      = "Player_Mov6_Aud";
+    public static readonly string   PLAYER_MOV7      = "Player_Mov7_Aud";
+    public static readonly string   PLAYER_MOV8      = "Player_Mov8_Aud";
+    public static readonly string   PLAYER_MOV9      = "Player_Mov9_Aud";
+    public static readonly string   PLAYER_HIT       = "Player_H_Aud";
+    public static readonly string   PLAYER_DEATH     = "Player_D_Aud";
+    public static readonly string   PLAYER_CONSUME   = "Player_Cons_Aud";
+    public static readonly string   PLAYER_POSESION  = "Player_Pos_Aud";
+    public static readonly string   ENEMY_DEATH1     = "Ene1_D_1_Aud";
+    public static readonly string   ENEMY_DEATH2     = "Ene1_D_2_Aud";
+    public static readonly string   ENEMY_DEATH3     = "Ene1_D_3_Aud";
+    public static readonly string   ENEMY_FIRE1      = "Ene1_F_1_Aud";
+    public static readonly string   ENEMY_FIRE2      = "Ene1_F_2_Aud";
+    public static readonly string   ENEMY_HIT1       = "Ene1_H_1_Aud";
+    public static readonly string   ENEMY_HIT2       = "Ene1_H_2_Aud";
 }
 public class AppSortingLayers
 {
