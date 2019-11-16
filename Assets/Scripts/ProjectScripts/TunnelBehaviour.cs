fef642b2e2425bbd1481a9a92ff82136467420a2
Daniel González Robayna <gonrobd@gmail.com>
2019-11-16 15:09:45 +0100

    Cambios en close y reañadido el blockeo de los túneles.

--- Assets/Scripts/ProjectScripts/TunnelBehaviour.cs
+++ Assets/Scripts/ProjectScripts/TunnelBehaviour.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class TunnelBehaviour : MonoBehaviour
 {
-
+    [SerializeField]
+    BoxCollider2D m_Collider2D;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.GetComponent<PossessAbility>() != null) ///Check de player mediante si tiene PossessAbility, que solo pertenece a Player.
         {
             GameManager.Instance.playerIsHidden = true;
+            m_Collider2D.enabled = false;
+        }
+        else
+        {
+            m_Collider2D.enabled = true;
         }
     }
 
