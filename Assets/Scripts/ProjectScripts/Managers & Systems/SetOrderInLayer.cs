087b571a6332322164e1a9bb96b6e72856fde3c8
Disader <sssergiosaez@gmail.com>
2019-11-19 12:25:15 +0100

    Arreglo de los enemigos que se quedan atrapados en las paredes y cambio en el sorting layer para changer que sea más preciso.

--- Assets/Scripts/ProjectScripts/Managers & Systems/SetOrderInLayer.cs
+++ Assets/Scripts/ProjectScripts/Managers & Systems/SetOrderInLayer.cs
@@ -13,6 +13,6 @@ public class SetOrderInLayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        mySprite.sortingOrder = -Mathf.RoundToInt(transformRef.position.y);
+        mySprite.sortingOrder = -Mathf.RoundToInt(transformRef.position.y*1000f);
     }
 }
