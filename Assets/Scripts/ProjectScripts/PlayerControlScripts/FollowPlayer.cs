d0b3722533645d0b16775487f2e8ec488838bac6
Disader <sssergiosaez@gmail.com>
2019-12-01 12:51:12 +0100

    La cámara se mueve en la dirección de apuntado con los controles de PC.

--- Assets/Scripts/ProjectScripts/PlayerControlScripts/FollowPlayer.cs
+++ Assets/Scripts/ProjectScripts/PlayerControlScripts/FollowPlayer.cs
@@ -5,18 +5,12 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     [SerializeField] float m_velocity;
-    protected PlayerInputAsset actions;
     [SerializeField] float m_aimDistance;
-    protected virtual void Start()
-    {
-
-        actions = new PlayerInputAsset();
-        actions.PlayerInputActions.Enable();
-    }
+ 
     // Update is called once per frame
     void Update()
     {
-        Vector2 desiredPosition= (Vector2)GameManager.Instance.ActualPlayerController.transform.position + actions.PlayerInputActions.Rotating.ReadValue<Vector2>().normalized * m_aimDistance;
+        Vector2 desiredPosition= (Vector2)GameManager.Instance.ActualPlayerController.transform.position + InputManager.Instance.RotationInput().normalized * m_aimDistance;
         transform.position = Vector2.MoveTowards(transform.position, desiredPosition, m_velocity * Time.deltaTime);
     }
 }
